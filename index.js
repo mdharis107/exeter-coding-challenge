6380bc31eafd08e49cd917ae5687fd084ff0ca44
HARIS#10# <mdharishz107@gmail.com>
2023-06-07 22:37:35 +0530

    created the translation file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 
-// Read the input files
 const initialFile = fs.readFileSync("t8.shakespeare.txt", "utf-8");
 
 const findWordsFile = fs.readFileSync("find_words.txt", "utf-8");
@@ -8,7 +7,6 @@ const findWords = findWordsFile.split("\n");
 
 const dictionaryFile = fs.readFileSync("french_dictionary.csv", "utf-8");
 
-// Load the updatedWords into a lookup table
 const updatedWords = {};
 const lines = dictionaryFile.split("\n");
 for (const line of lines) {
@@ -16,10 +14,8 @@ for (const line of lines) {
   updatedWords[englishWord] = frenchWord;
 }
 
-// Track replaced words and their frequencies
 const convertedWords = {};
 
-// Replace English words with French translations
 let translatedText = initialFile;
 
 for (const word of findWords) {
@@ -34,10 +30,8 @@ for (const word of findWords) {
   }
 }
 
-// Save the processed text file
 fs.writeFileSync("t8.shakespeare.translated.txt", translatedText);
 
-// Create frequency.csv
 let frequencyFile = "English word,French word,Frequency\n";
 for (const [englishWord, frequency] of Object.entries(convertedWords)) {
   const frenchWord = updatedWords[englishWord];
@@ -46,7 +40,6 @@ for (const [englishWord, frequency] of Object.entries(convertedWords)) {
 fs.writeFileSync("frequency.csv", frequencyFile);
 
 
-// Measure time taken and memory used
 const performanceData = process.resourceUsage();
 const timeTaken = `${performanceData.userCPUTime / 1000000} seconds`;
 const memoryUsed = `${performanceData.maxRSS / 1024 / 1024} MB`;
